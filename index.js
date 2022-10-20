4488edfbf8c8f3e5791d1f7e2f8e6152bd8ba257
Yaroslav Kasperovych <yaroslav.kasperovych@hopin.com>
2022-10-20 22:51:43 +0200

    fix(scope): support dashes

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ const dangerousRegistries = [
   "https://registry.yarnpkg.com",
   "https://registry.yarnpkg.com/",
   "https://registry.npmjs.org/",
-  "https://registry.npmjs.org"
+  "https://registry.npmjs.org",
 ];
 
 const npmConfigRegistry = process.env.npm_config_registry;
 const isNpmConfigRegistrySet = typeof npmConfigRegistry === "string";
-const isNpmConfigRegistryDangerous = !isNpmConfigRegistrySet || dangerousRegistries.includes(npmConfigRegistry);
+const isNpmConfigRegistryDangerous =
+  !isNpmConfigRegistrySet || dangerousRegistries.includes(npmConfigRegistry);
 
 const packageName = process.env.npm_package_name;
 const isPackageNameSet = typeof packageName === "string";
 const packageScope =
   isPackageNameSet && packageName[0] === "@"
-    ? packageName.substring(1, packageName.indexOf("/"))
+    ? packageName.substring(1, packageName.indexOf("/")).replace(/-/g,   "_")
     : null;
-
 
 const secureScope = () => {
   const scopeRegistry = process.env[`npm_config__${packageScope}_registry`];
   const isScopeRegistrySet = typeof scopeRegistry === "string";
-  const isScopeRegistryDangerous = isScopeRegistrySet && dangerousRegistries.includes(scopeRegistry);
+  const isScopeRegistryDangerous =
+    isScopeRegistrySet && dangerousRegistries.includes(scopeRegistry);
 
   if (isScopeRegistryDangerous) {
     throw new errors.ScopeRegistryDangerousError();
@@ -33,13 +34,13 @@ const secureScope = () => {
   if (!isScopeRegistrySet && isNpmConfigRegistryDangerous) {
     throw new errors.ScopeNotSetNpmConfigRegistryDangerousError();
   }
-}
+};
 
 const secureFromDangerousRegistries = () => {
   if (isNpmConfigRegistryDangerous) {
     throw new errors.NpmConfigRegistryDangerousError();
   }
-}
+};
 
 const securePublish = ({ useScope }) => {
   if (useScope) {
